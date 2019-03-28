f3283f6c38ebd72746ab27e11da0f501e08ce066
AbdelrahmanAshraf9079 <abdelrahman9709@gmail.com>
2019-03-28 15:07:09 +0200

    MileStone3

--- app/src/main/java/com/example/weatherapp/DeviceDetailActivity.java
+++ app/src/main/java/com/example/weatherapp/DeviceDetailActivity.java
@@ -2,21 +2,53 @@ package com.example.weatherapp;
 
 import  android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 
 public class DeviceDetailActivity extends AppCompatActivity {
 
-   // private ClientThread thread;
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference myRef = database.child("ESP1");
+    private static final String TAG = "READ";
+    // private ClientThread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_device_detail);
 
 
-        TextView Text = (TextView)findViewById(R.id.Text) ;
+        final TextView Text = (TextView)findViewById(R.id.Text) ;
+
+        Text.append("Temperature History\n");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                double value = dataSnapshot.getValue(double.class);
+
+                Log.d( TAG,"Value is: " + value);
+
+                Text.append(value + "\n");
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w( TAG , "Failed to read value.", error.toException());
+            }
+        });
 
         
     }
